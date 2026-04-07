45b75cd60fb72270b62081ca33a0ee8f24262f01
신건하 <gunha1215@gmail.com>
2026-04-07 10:05:07 +0900

    배포

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -34,7 +34,7 @@ export default function IntroOverlay({ onDone }: { onDone?: () => void }) {
       if (fadeTimeout) {
         clearTimeout(fadeTimeout);
       }
-    };
+    };  
   }, [onDone]);
 
   return (
